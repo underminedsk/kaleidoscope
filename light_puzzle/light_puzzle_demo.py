e4d8bb4f16e04bcc71e939b900cf4502dab2c492
zkozick <zkozick@nanigans.com>
2016-05-19 14:11:21 -0700

    add light puzzle python files

---
+++ light_puzzle/light_puzzle_demo.py
@@ -0,0 +1,67 @@
+MAX_ALLOWED_MOVES = 9999
+NUM_NODES = 3
+
+#STATES = ['RED', 'ORANGE', 'YELLOW']
+STATES = ['RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE']
+
+
+def initial_game_state():
+    return [STATES[0] for i in range(0,NUM_NODES)]
+
+
+def get_user_input():
+    choice = raw_input('choose a node (1-%s)):' % (NUM_NODES))
+    try:
+        choice = int(choice)
+        if choice <=0 or choice > NUM_NODES:
+            raise ValueError()
+        else:
+            return choice
+    except:
+        print 'ERROR: enter a number between 1 and %s' % NUM_NODES
+        return None
+
+
+def next_node_state(state):
+    idx = STATES.index(state)+1
+    return STATES[idx] if idx < len(STATES) else STATES[0]
+
+
+def next_game_state(current_game_state, user_choice):
+    next_state = []
+    for node_idx in range(0, NUM_NODES):
+        cur_node_state = current_game_state[node_idx]
+        if node_idx+1 != user_choice:
+            next_state.append(next_node_state(cur_node_state))
+        else:
+            next_state.append(cur_node_state)
+    return next_state
+
+
+def puzzle_solved(current_game_state):
+    for node_state in current_game_state:
+        if node_state != STATES[-1]:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    print 'rules: (1) all nodes start as %s' % STATES[0]
+    print 'rules: (2) win make all the nodes %s' % STATES[-1]
+    print '       (3) choosing a node changes the color of the other nodes. '
+    print '       (4) node colors are %s' % ' -> '.join(STATES)
+    moves = 0
+    current_game_state = initial_game_state()
+    while moves < MAX_ALLOWED_MOVES:
+        print current_game_state
+        moves += 1
+        user_choice = get_user_input()
+        if user_choice:
+            current_game_state = next_game_state(current_game_state, user_choice)
+            if puzzle_solved(current_game_state):
+                print 'Success! You solved the puzzle in %s moves' % moves
+                break
+
+
+
+
---
+++ light_puzzle/light_puzzle_simulator.py
@@ -0,0 +1,33 @@
+import sys
+
+MAX_STATE=5
+NUM_NODES=3
+
+#initialize initial state to all zeros
+initial_node_state = [0 for i in range(0, NUM_NODES)]
+
+#only one possible initial node state: all zeros
+possible_states = [initial_node_state]
+print 'hit enter to see all possible states at next level'
+while 1: #loop forever
+    raw_input()
+    #compute all new possible states
+    print len(possible_states), 'possible states'
+    print possible_states
+    new_states = []
+    for state in possible_states: #for each current possible state of the system...
+        for j in range(0,NUM_NODES): #if the user activates node 'j'...
+            new_state = []
+            for state_idx in range(0,NUM_NODES): #loop through nodes in current state
+                #if this is the node user activated, then leave value the same, otherwise increase state by 1
+                new_value_for_idx = state[state_idx]+1 if state_idx != j else state[state_idx]
+                #if we've reached max state, drop back to zero.
+                if new_value_for_idx == MAX_STATE:
+                    new_value_for_idx = 0
+                new_state.append(new_value_for_idx)
+            new_states.append(new_state) #add computed state to possible states
+    possible_states = new_states
+
+
+
+
