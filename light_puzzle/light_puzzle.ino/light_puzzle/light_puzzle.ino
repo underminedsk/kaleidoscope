ae07b89b757b64685da709d016ec997e98a271d1
Zach Kozick <zachkozick@gmail.com>
2016-06-01 08:54:05 -0700

    update light puzzle code

--- light_puzzle/light_puzzle.ino/light_puzzle/light_puzzle.ino
+++ light_puzzle/light_puzzle.ino/light_puzzle/light_puzzle.ino
@@ -10,9 +10,12 @@ MFRC522 rfid(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key; 
 
 //game constants
-#define NUM_NODES 3 //this game has 3 nodes
-#define TIMEOUT 60 //user has this many seconds to solve the game, after which it returns to idle.
-char COLORS[3][8] = {"RED", "ORANGE", "YELLOW"};
+const int NUM_NODES = 3; //this game has 3 nodes
+const int ALLOWED_TIME_FOR_PUZZLE = 60000; //user has this many milliseconds to solve the game, after which it returns to idle.
+const int NUM_COLORS = 3; //TODO can I get this at runtime?
+const int MAX_COLOR_LEN = 8; //maximum number of characters of any color 
+const char COLORS[NUM_COLORS][MAX_COLOR_LEN] = {"RED", "ORANGE", "YELLOW"};
+const int button_pins[NUM_NODES] = {1, 2, 3}; //holds the pin ids of the buttons
 
 //array to store user id.  sometimes the user_id will be null, signifying that the game
 //is in the idle state.
@@ -21,7 +24,10 @@ byte nuid[3];
 //array to store the current state of the puzzle. the outputs of game (lights)
 //will be updated to match the values in this array using the 'publish_current_state'
 //function below.
-char puzzle_state[NUM_NODES][6]; 
+char puzzle_state[NUM_NODES][MAX_COLOR_LEN]; 
+
+//holds the start time of the puzzle
+unsigned long puzzle_start_time;
 
 //global state setting functions.  the state is either 'idle' or 'active'.  The program determines
 //if it's active by whether the first byte of the nuid == '\0'
@@ -37,6 +43,7 @@ void set_active_state() {
   for (int i=0; i<NUM_NODES; i++) {
     strcpy(puzzle_state[i], COLORS[0]);
   }
+  puzzle_start_time = millis();
 }
 
 bool read_nuid_from_rfid_card() {
@@ -54,12 +61,96 @@ bool read_nuid_from_rfid_card() {
   return true;
 }
 
+unsigned long elapsed_time() { 
+  return millis() - puzzle_start_time; 
+}
+
+bool puzzle_is_solved() {
+  for (int i=0; i<NUM_NODES; i++) { 
+    if (strcmp(puzzle_state[i], COLORS[NUM_COLORS-1]) != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+long last_debounce_times[NUM_NODES]; //store the last debounce times for each node 
+int last_button_states[NUM_NODES]; //the last state read from the buttons.
+const long DEBOUNCE_TIME = 1000; 
+/*
+ * returns true if a node has been activated by the user.  this implementation uses digital buttons.
+ */
+bool is_node_activated(int button_index) {
+  int button_state = digitalRead(button_pins[button_index]);
+
+  // If the switch has changed state since last time we checked, record the time
+  if (button_state != last_button_states[button_index]) {
+    last_debounce_times[button_index] = millis();
+  }
+
+  bool registered_button_press = false;
+  if ((millis() - last_debounce_times[button_index]) > DEBOUNCE_TIME) {
+    //ok, reading hasn't changed for longer than the debounce time, so it must 
+    //not be noise.   
+    if (button_state == HIGH) {
+      log("button has changed state to high");
+    } else if (button_state == LOW) {
+      log("button has changed state to low");
+    }
+
+    if (button_state == HIGH) {
+      registered_button_press = true;
+    }
+  }
+
+  //next time through the loop, the current button state is the last.
+  last_button_states[button_index] = button_state;
+
+  return registered_button_press;
+}
+
+/*
+ * given that the node has been activated, update the puzzle state.  Algorithm is: whatever node is 
+ * activated, the other nodes change by N colors, where N is the distance 
+ */
+void update_puzzle_state_for_node_activated(int node_id) {
+  for (int i=0; i<NUM_NODES; i++) {  
+    int distance = abs(node_id - i);
+    if (distance > 0) {
+      update_node_state(i, distance);     
+    }
+  }
+}
+
+/*
+ * change the given node by the specified number of color states.   
+ */
+void update_node_state(int node_id, int num_colors_to_change_by) { 
+   int cur_idx_of_color = get_index_of_color(puzzle_state[node_id]);
+   int new_idx_of_color = (cur_idx_of_color + num_colors_to_change_by) % NUM_COLORS;
+   strcpy(puzzle_state[node_id], COLORS[new_idx_of_color]);
+}
+
+int get_index_of_color(char color[]) {
+  for (int i=0; i<NUM_COLORS; i++) {
+    if (strcmp(color, COLORS[i]) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
 
+//  for (int i=0; i<NUM_NODES; i++) {
+//    last_button_states[i] = LOW; //set last button state to off.
+//    pinMode(button_pins[i], INPUT); //initialize button pins for reading
+//  } 
+  
   set_idle_state();
   log("setup complete");
 }
@@ -73,20 +164,34 @@ void loop() {
       publish_current_puzzle_state();
     } 
   } else {
-    //puzzle logic here
+    //did we time out?
+    if (elapsed_time() > ALLOWED_TIME_FOR_PUZZLE) { 
+      publish_puzzle_timed_out();  
+      set_idle_state(); 
+    } 
+
+    //is node button currently being pressed?
+    for (int i=0; i<NUM_NODES; i++) {
+      if (is_node_activated(i)) {
+         update_puzzle_state_for_node_activated(i);
+         publish_current_puzzle_state();
+         delay(100);
+         break; //dont process any more buttons this round.
+      }
+    } 
+
+    if (puzzle_is_solved()) { //has the user solved the puzzle? 
+      publish_puzzle_solved();
+      set_idle_state(); 
+    } 
   }
   
 }
 
-void log(char msg[]) {
-  Serial.println(msg);
-}
 
-void log_partial(char msg[]) {
-  Serial.print(msg);
-}
 
-// ---- puzzle state publishing functions.  these should update the lights to reflect the current state  ---- 
+
+// ---- puzzle state publishing functions.  these should update the lights to reflect the current internal state  ---- 
 void publish_puzzle_started() {
   //this might actually do nothing, or might just be the same as publish puzzle state
   log("puzzle has been activated");  
@@ -101,13 +206,24 @@ void publish_puzzle_timed_out() {
 }
 
 void publish_current_puzzle_state() {
-
+  log_partial("publishing current state: ");
   //just print out the current state of the game 
   for (int i=0; i<NUM_NODES; i++) {
     log_partial(puzzle_state[i]);
     log_partial(" ");
   }
-  log("");
+  log(""); // line break
+} 
+
+// --- logging functions- just wrappers around serial.print
+void log(char msg[]) {
+  //logs a message using Serial.println()
+  Serial.println(msg);
+}
+
+void log_partial(char msg[]) {
+  //logs a partial message using Serial.print()
+  Serial.print(msg);
 }
 
 
--- light_puzzle/light_puzzle_simulator.py
+++ light_puzzle/light_puzzle_simulator.py
@@ -1,35 +1,47 @@
 import sys
 
 import math
-
-MAX_STATE=5
+ 
 NUM_NODES=3
 
 #initialize initial state to all zeros
-initial_node_state = [0 for i in range(0, NUM_NODES)]
+COLORS = ['RED', 'ORANGE', 'YELLOW']
+initial_node_state = [COLORS[0] for i in range(0, NUM_NODES)]
+
 
 #only one possible initial node state: all zeros
 possible_states = [initial_node_state]
+
+print len(possible_states), 'possible states'
+print possible_states
+raw_input()
+
+
 print 'hit enter to see all possible states at next level'
 while 1: #loop forever
-    raw_input()
     #compute all new possible states
-    print len(possible_states), 'possible states'
-    print possible_states
     new_states = []
     for state in possible_states: #for each current possible state of the system...
         for j in range(0,NUM_NODES): #if the user activates node 'j'...
             new_state = []
             for state_idx in range(0,NUM_NODES): #loop through nodes in current state
-                #if this is the node user activated, then leave value the same, otherwise increase state by 1
-                new_value_for_idx = state[state_idx]+abs(state_idx-j)
+                #if this is the node user activated, then leave value the same, otherwise increase state by the distance from the node
+                cur_color = COLORS[state_idx] 
+                cur_idx_of_color = COLORS.index(cur_color)
+                new_index_of_color_before_mod = cur_idx_of_color+abs(state_idx-j)
+                new_idx_of_color = new_index_of_color_before_mod % len(COLORS) if new_index_of_color_before_mod >= len(COLORS) else new_index_of_color_before_mod
+                new_value_for_idx = COLORS[new_idx_of_color]
+                print 'node_activated=%s, state_idx=%s, cur_color=%s, cur_idx_of_color=%s, new_idx_of_color_before_mod=%s, new_idx_of_color=%s, new_value_for_idx=%s' % (j, state_idx, cur_color, cur_idx_of_color, new_index_of_color_before_mod, new_idx_of_color, new_value_for_idx)
                 #new_value_for_idx = state[state_idx]+1 if state_idx != j else state[state_idx]
-                #if we've reached max state, drop back to zero.
-                if new_value_for_idx >= MAX_STATE:
-                    new_value_for_idx = 0
+                #if we've reached max state, drop back to zero. 
                 new_state.append(new_value_for_idx)
             new_states.append(new_state) #add computed state to possible states
     possible_states = new_states
+
+    print len(possible_states), 'possible states'
+    print possible_states
+
+    raw_input()
 
 
 
