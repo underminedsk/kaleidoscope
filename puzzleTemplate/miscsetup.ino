aec0ed574544da2496511ea975cd926863c9556a
BanjoIsland <waysern@gmail.com>
2016-07-13 18:05:37 -0700

    adding puzzle template
    customize puzzle.ino depending on puzzle

---
+++ puzzleTemplate/miscsetup.ino
@@ -0,0 +1,176 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+// Addr: 0x3F, 20 chars & 4 lines. Sometimes display boards use address 0x27
+LiquidCrystal_I2C lcd(0x3F, 16, 2); //Frentally display, use 0x3F if not working try 0x27
+
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void playTone() {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(A2, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(A2);
+  }
+}
+
+void lcdSetup()
+{
+  lcd.clear();
+  lcd.init();
+  delay(100);
+  lcd.noBacklight();
+  lcd.setCursor(0, 0);
+  firstLine = "Hello";
+  secondLine = "What's your name?";
+  thirdLine = "";
+  fourthLine = "";
+}
+
+void lcdDisplayOn() {
+  lcd.backlight();
+  //lcd.blink();
+}
+
+
+
+boolean nameEntered() {
+
+  byte letter[1];
+
+  return false;
+}
+
+void updateLcd(byte val, byte &letterNum) {
+  switch (val) {
+    case 8: // Backspace
+      if (letterNum > 0) {
+        letterNum--;
+        playerName[letterNum] = 0;
+        thirdLine = (char*)playerName;
+      }
+      break;
+
+    case 13: // Enter
+      firstLine = "Your name is:";
+      secondLine = (char*)playerName;
+      thirdLine = "";
+      fourthLine = "Please retag card";
+      lcd.noBlink();
+      break;
+
+    default:
+      if (val >= 32 && val <= 122 && letterNum < 15) {
+        playerName[letterNum] = val;
+        letterNum++;
+        
+      }
+      thirdLine = (char*)playerName;
+  }
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(firstLine);
+  lcd.setCursor(0, 1);
+  lcd.print(secondLine);
+  lcd.setCursor(0, 2);
+  lcd.print(thirdLine);
+  lcd.setCursor(0, 3);
+  lcd.print(fourthLine);
+  lcd.setCursor(letterNum, 2);
+}
+
+void lcdMessage(String first, String second) {
+  lcd.clear();  
+  lcd.setCursor(0, 0);
+  lcd.print(first);
+  lcd.setCursor(0, 1);
+  lcd.print(second);
+}
+
+#include <Keypad.h>
+
+#define Password_Lenght 7 // Give enough room for six chars + NULL char
+
+char Data[Password_Lenght]; // 6 is the number of chars it can hold + the null char = 7
+char Master[Password_Lenght] = "123456"; 
+byte data_count = 0, master_count = 0;
+bool Pass_is_good;
+char customKey;
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'0','1','2','3'},
+  {'4','5','6','7'},
+  {'8','9','A','B'},
+  {'C','D','E','F'}
+};
+byte rowPins[ROWS] = {A1, 0, 9, 8}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad 
+Keypad customKeypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void keypadSetup()
+{
+  lcd.setCursor(0,0);
+  lcd.print("Enter Password");
+
+  customKey = customKeypad.getKey();
+  if (customKey) // makes sure a key is actually pressed, equal to (customKey != NO_KEY)
+  {
+    Data[data_count] = customKey; // store char into data array
+    lcd.setCursor(data_count,1); // move cursor to show each new char
+    lcd.print(Data[data_count]); // print char at said cursor
+    data_count++; // increment data array by 1 to store new char, also keep track of the number of chars entered
+  }
+
+  if(data_count == Password_Lenght-1) // if the array index is equal to the number of expected chars, compare data to master
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Password is ");
+
+    if(!strcmp(Data, Master)) {// equal to (strcmp(Data, Master) == 0)
+      lcd.print("Good");
+      gameDone = true; 
+    }
+    else
+      lcd.print("Bad");
+
+    delay(1000);// added 1 second delay to make sure the password is completely shown on screen before it gets cleared.
+    lcd.clear();
+    clearData();   
+  }
+}
+
+void clearData()
+{
+  while(data_count !=0)
+  {   // This can be used for any array size, 
+    Data[data_count--] = 0; //clear array for new data
+  }
+  return;
+}
---
+++ puzzleTemplate/puzzleTemplate.ino
@@ -0,0 +1,140 @@
+#define Sprintln(a) (Serial.println(F(a)))
+//#define Sprintln(a)
+
+#define WAIT_FOR_TAG 0
+#define WAIT_FOR_GAME_DONE 1
+#define GAME_DONE 2
+#define WRITE_TAG 3
+#define PLAY_VIDEO 4
+uint8_t checkin_fsm_state = WAIT_FOR_TAG;
+uint8_t puzzle_state = 0;
+
+#define checkin_tries_byte 0
+#define speech_tries_byte 1
+#define light_tries_byte 2 
+#define checkin_done_byte 4
+#define speech_done_byte 5
+#define speech_puzzlenum_byte 9 
+#define name_rfid_block 4 
+#define data_rfid_block 5 
+
+#include <FastLED.h>
+
+#define LED_PIN     A3
+#define COLOR_ORDER GRB
+#define CHIPSET     WS2811
+#define NUM_LEDS    11
+
+#define BRIGHTNESS  200
+#define FRAMES_PER_SECOND 100
+CRGB leds[NUM_LEDS];
+bool gReverseDirection = false;
+
+
+
+byte letterNum;
+
+
+String firstLine = "Hello seeker";
+String secondLine = "What is your name?";
+String thirdLine = "";
+String fourthLine = "";
+
+//boolean rfidDetected(byte uid[], byte playerName[], byte playerData[]);
+
+byte playerUid[4];  
+byte playerName[16];
+byte playerData[16];
+const int playerPacketSize = sizeof(playerUid) + sizeof(playerName) + sizeof(playerData);
+byte playerPacket[playerPacketSize];
+
+boolean gameDone = false;
+
+void setup()
+{
+  Serial.begin(9600); // Initialize serial communications with the PC
+  rfidSetup();
+  lcdSetup();
+  ledSetup();
+}
+
+void loop() {
+  //state machine
+  switch (checkin_fsm_state)
+  {
+    case WAIT_FOR_TAG:  //Starting state to wait for RFID tag
+      Fire2012(); // run simulation frame
+      FastLED.show(); // display this frame
+      FastLED.delay(1000 / FRAMES_PER_SECOND); 
+      
+      if (rfidDetected(playerUid, playerName, playerData, checkin_tries_byte)) {
+        //String welcomeMsg  = "Welcome," + String((char*)playerName); 
+        
+        // increment check in tries on rfid tag
+        //if (!incrementBlockRfid(checkin_tries_byte)) {break;} 
+        gameDone = false; 
+        lcdDisplayOn();
+        lcdMessage("Welcome", String((char*)playerName));
+        Sprintln("inc successful");
+        checkin_fsm_state = WAIT_FOR_GAME_DONE;
+        //dump_byte_array(playerPacket, playerPacketSize);          
+
+      } else {
+        closeRfid();
+      }  
+      break;
+
+    case WAIT_FOR_GAME_DONE:
+      updateGameState();
+      
+      if (gameDone == true) {
+        checkin_fsm_state = GAME_DONE;
+      }
+      break;  
+
+
+
+    case GAME_DONE:
+      // Display name and ask to retag
+      byte oldPlayerName[16];
+      byte oldPlayerData[16];
+      byte writeTagDone[1];
+      lcdMessage("Please retag","your card");
+
+      if (rfidDetected(playerUid, oldPlayerName, oldPlayerData, checkin_done_byte)) {
+        //if (!writeRfid(playerData, data_rfid_block, 16)) {break;}
+
+        
+        
+        lcdSetup();
+        checkin_fsm_state = WAIT_FOR_TAG;
+      } 
+      closeRfid();
+      break;
+
+    default:
+      break;
+  }
+  printState(); //Sprintln(checkin_fsm_state);
+  delay(100);              // wait for 100 ms
+}
+
+void printState()
+{
+  switch (checkin_fsm_state) {
+    case WAIT_FOR_TAG: Sprintln("WAIT_FOR_TAG"); break;
+    case WAIT_FOR_GAME_DONE: Sprintln("WAIT_FOR_GAME_DONE"); break;
+    case GAME_DONE: Sprintln("GAME_DONE"); break;
+    case WRITE_TAG: Sprintln("WRITE_TAG"); break;
+    case PLAY_VIDEO: Sprintln("PLAY_VIDEO"); break;
+    default: Sprintln("Unknown State"); break;   
+  }  
+}
+
+void createPlayerPacket(byte playerUid[], byte playerName[], byte playerData[], byte combined[]) {
+  // for some reason, using sizeof(variable) in memcpy messes up
+  memcpy(combined, playerUid, 4);
+  memcpy(combined+4, playerName, 16);
+  memcpy(combined+20, playerData, 16);
+}
+
